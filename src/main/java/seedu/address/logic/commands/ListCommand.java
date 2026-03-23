c358de690ae6637b18ef840dcc79719c560d157d
kah-seng <71002797+kah-seng@users.noreply.github.com>
2026-03-23 22:43:50 +0800

    Add ListCommandParser.java

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,6 +1,13 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_AGE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENT_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENT_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PARENT_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
@@ -14,6 +21,30 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists your contacts, and optionally sorts. "
+            + "Use at most one of the following parameters: "
+            + PREFIX_NAME + " | "
+            + PREFIX_AGE + " | "
+            + PREFIX_PARENT_NAME + " | "
+            + PREFIX_PARENT_PHONE + " | "
+            + PREFIX_PARENT_EMAIL + "\n"
+            + "Examples:\n"
+            + COMMAND_WORD + " " + PREFIX_NAME + "\n"
+            + COMMAND_WORD;
+
+    public enum SortParameter {
+        NAME, AGE, PARENT_NAME, PARENT_PHONE, PARENT_EMAIL
+    }
+
+    private final SortParameter sortParameter;
+
+    public ListCommand() {
+        this.sortParameter = null;
+    }
+
+    public ListCommand(SortParameter sortParameter) {
+        this.sortParameter = sortParameter;
+    }
 
     @Override
     public CommandResult execute(Model model) {
